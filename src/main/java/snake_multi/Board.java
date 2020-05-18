507127d75c77e6c0771eea4a7d028e2db59d59b8
jedrzej_cieslikiewicz <jdrq44@gmail.com>
2020-05-18 16:05:00 +0200

    drugi commit

---
+++ src/main/java/snake_multi/Board.java
@@ -0,0 +1,9 @@
+package snake_multi;
+
+
+
+public class Board extends JPanel implements ActionListener {
+    public Board(){
+
+    }
+}
---
+++ src/main/java/snake_multi/Snake.java
@@ -0,0 +1,11 @@
+package snake_multi;
+
+public class Snake {
+    public Snake(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
